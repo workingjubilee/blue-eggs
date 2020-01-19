a5b4c51cc4c9fbe5cad1b8a003bd4cd7d2ab7253
Jubilee Young <workingjubilee@gmail.com>
2020-01-18 16:08:48 -0800

    Boilerplate for asset loading/editing

--- BlueEggs/ModEntry.cs
+++ BlueEggs/ModEntry.cs
@@ -7,11 +7,56 @@ namespace BlueEggs
     // this mod is deliberately constructed as an experiment in modding AND
     // so that it is not dependent on other mods for running in the game!
     // multiple dependencies seem to be required, this acts in one swoop!
-    public class ModEntry : Mod
+    public class ModEntry : Mod, IAssetEditor, IAssetLoader
     {
+        bool IAssetEditor.CanEdit<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Data/ObjectInformation") ? true : false;
+        }
 
-        // TODO: add CanLoad
-        // TODO: add AssetLoader
+        /// Edit a matched asset.
+        /// A helper which encapsulates metadata about an asset and enables changes to it.
+        void IAssetEditor.Edit<T>(IAssetData asset)
+        {
+            if (asset != null && asset.AssetNameEquals("Data/ObjectInformation"))
+            {
+                //IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
+                //foreach (int itemID in data.Keys)
+                //{
+                //    string[] fields = data[itemID].Split('/');
+                //    fields[1] = (int.Parse(fields[1]) * 2).ToString();
+                //    data[itemID] = string.Join("/", fields);
+                //}
+
+
+                //public void Edit<T>(IAssetData asset)
+                //{
+                //    IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
+                //    data[999] = "... some new item string ...";
+                //}
+            }
+        }
+
+        /// Get whether this instance can load the initial version of the given asset.
+        /// Basic metadata about the asset being loaded.
+        bool IAssetLoader.CanLoad<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Objects/BlueEggs");
+        }
+
+        public T Load<T>(IAssetInfo asset)
+        {
+            if (asset != null && asset.AssetNameEquals("Objects/BlueEggs"))
+            {
+                return Helper.Content.Load<T>("assets/BlueEggs.png", ContentSource.ModFolder);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+            }
+        }
+
+
         // TODO: slice sprites out as Texture2Ds for loading with blue eggs
         // TODO: add blue eggs to game!
         // TODO: impl and test generic egg traits
@@ -26,5 +71,12 @@ namespace BlueEggs
         {
             // Successful minimum build!
         }
+
+        //// read an image file
+//        Texture2D texture = helper.Content.Load<Texture2D>("assets/texture.png", ContentSource.ModFolder);
+
+        // read a json
+//IDictionary<string, string> data = helper.Content.Load<Dictionary<string, string>>("assets/data.json", ContentSource.ModFolder);
+
     }
 }
